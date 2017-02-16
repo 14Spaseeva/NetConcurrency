0b0a1a1ba40ac56eb7e333ecc5724ecb6521ae6a
14Spaseeva <stasiaspaseeva@gmail.com>
2017-02-17 01:27:34 +0300

    Домашнее задание 1

--- src/Client.java
+++ src/Client.java
@@ -5,13 +5,19 @@ import java.net.Socket;
  * Created by 14Spaseeva on 10.02.2017.
  */
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // в арг пишем хост и порт
         try {
-            Socket socket = new Socket("localhost", 2000);
+
+            Socket socket = new Socket(args[0], Integer.parseInt( args[1]));
             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            dataOutputStream.writeUTF("Hello world");
 
-
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String message = "";
+            while (!message.equals("exit")) {
+                message = bufferedReader.readLine();
+                dataOutputStream.writeUTF(message);
+                System.out.println("message is sent");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/Server.java
+++ src/Server.java
@@ -6,19 +6,22 @@ import java.net.Socket;
  * Created by 14Spaseeva on 10.02.2017.
  */
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //в аргумент пишем номер порта
         ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(2000);
+            serverSocket = new ServerSocket( Integer.parseInt( args[0]));
             Socket socket = serverSocket.accept ();
 
             DataInputStream dataInputStream = new  DataInputStream(socket.getInputStream());
 //            DataOutputStream dataOutputStream = new  DataOutputStream(socket.getOutputStream());
 //            BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( System.in));
-            String message = dataInputStream.readUTF();
-            System.out.println(message);
+            String message = "";
+            while (!message.equals("exit")) {
 
+                message = dataInputStream.readUTF();
+                System.out.println(message);
 
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
